0c19a0e5c83cd7f9656af3b6bb66cc458b62d5db
Don <don.qcastillo@yahoo.com>
2026-03-13 02:37:27 -0600

    implemented the GET /review/:isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,8 +42,9 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-	//Write your code here
-	return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  let reviews = books[isbn] ? books[isbn]?.reviews : {};
+	return res.status(200).json(reviews);
 });
 
 module.exports.general = public_users;
