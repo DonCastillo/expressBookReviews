e1da76aa52f9389b841e94f1f8505e43a2ede11c
Don <don.qcastillo@yahoo.com>
2026-03-13 02:16:05 -0600

    implemented GET /isbn/:isbn endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,13 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const bookSelected = books[req.params.isbn];
+  return res.status(300).send(bookSelected);
  });
   
 // Get book details based on author
